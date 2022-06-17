9d6f11c8bae87df03e3e139725940d5bb3d6c84c
aLe3ouLa <ale3mp90@gmail.com>
2022-06-17 17:42:02 +0200

    add: bubble sort optimization

--- algorithms/sorts/bubble_sort_opt.js
+++ algorithms/sorts/bubble_sort_opt.js
@@ -1,14 +1,22 @@
 const bubbleSort = (arr) => {
-  if (!arr) return;
+  let swapped;
   for (let i = 0; i < arr.length; i++) {
+    swapped = false;
     for (let j = 0; j < arr.length - i; j++) {
       if (arr[j] > arr[j + 1]) {
         const temp = arr[j];
-        arr[j] = arr[j + 1]
+        arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        swapped = true;
       }
     }
+
+    // IF no two elements were
+    // swapped by inner loop, then break
+    if (swapped == false)
+      break;
   }
+
   return arr
 }
 
