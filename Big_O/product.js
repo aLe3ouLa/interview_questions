7406a6aa8a3ba4e66795e96f2bcf467d5449884e
Alexandra Barka <ale3mp90@gmail.com>
2024-05-30 14:14:31 +0200

    Update product.js

--- Big_O/product.js
+++ Big_O/product.js
@@ -1,11 +1,11 @@
 function product(a,b) {
   let sum = 0; // o(1)
   for (let i = 0; i < b; i++){
-    sum = sum + a; //o(1) * n
+    sum = sum + a; //o(1) * b
   }
   return sum; // o(1)
 }
 
 product(5,5);
 
-// complexity o(n)
+// complexity o(b)
